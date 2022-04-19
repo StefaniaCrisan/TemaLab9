b9ce52f5c467cbfc36041484ddd2e833d542e329
StefaniaCrisan <stefania.vlaic@yahoo.es>
2022-04-19 23:18:52 +0300

    exercitiul 1 + 2

--- src/ro/fasttrack/lab9/tema/PersonEx2.java
+++ src/ro/fasttrack/lab9/tema/PersonEx2.java
@@ -1,5 +1,5 @@
 package ro.fasttrack.lab9.tema;
 
 public class PersonEx2 {
-    //Employee angajat = new Employee((15,04,2021),"programmer");
+    // Employee angajat = new Employee((15,04,2021),"programmer");
 }
